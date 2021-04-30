67a3ce75339b95897bdee75b2a46dc349ee4e889
Kritika Pattabhi Raman <kritikawin@gmail.com>
2021-04-30 21:27:03 +0530

    stage 1 complete

--- src/get_data.py
+++ src/get_data.py
@@ -22,7 +22,7 @@ def get_data(config_path):
     #print(df.head())
     return df
 
-
+  
 if __name__=="__main__":
     args = argparse.ArgumentParser()
     args.add_argument("--config", default="params.yaml")
