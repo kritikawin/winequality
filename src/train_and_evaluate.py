0c9f9220ba9b5c6b4ef1fa124fe92d361f261566
Kritika Pattabhi Raman <kritikawin@gmail.com>
2021-05-01 09:55:14 +0530

    tracker added

--- src/train_and_evaluate.py
+++ src/train_and_evaluate.py
@@ -68,14 +68,14 @@ def train_and_evaluate(config_path):
             "mae": mae,
             "r2": r2
         }
-        
+        json.dump(scores, f, indent = 4)
 
     with open(params_file, "w") as f:
         params = {
-            "rmse": rmse,
-            "mae": mae,
-            "r2": r2
+            "alpha": alpha,
+            "l1_ratio": l1_ratio,
         }
+        json.dump(params, f, indent = 4)
         
 #####################################################
 
